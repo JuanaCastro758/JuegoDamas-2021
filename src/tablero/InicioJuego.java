1d16faeb36cd4c3c50fe86a25ca86baf25f01bd9
JuanaCastro758 <juanaazucenacastrojimenez@gmail.com>
2021-06-18 18:29:45 -0600

    controla las partidas

---
+++ src/tablero/InicioJuego.java
@@ -0,0 +1,9 @@
+package src.tablero;
+public class InicioJuego {
+    public InicioJuego(){
+       //vacio 
+    }
+    public void recibirJugadores(int j1, int j2){
+         System.out.println(j1+"  "+j2);
+    }
+}
